ff4557c5cad9f944619f715052f0ebd4bf554dd7
Jintzo <me@jintzo.me>
2017-03-18 23:32:28 +0100

    added authentication routes

---
+++ routes/auth.js
@@ -0,0 +1,91 @@
+// load required modules
+var express = require('exoress')
+var callbackFactory = require('./../factories/callback')
+
+// set up router
+var router = express.Router()
+
+// load auth model
+var auth = require('./../models/auth')
+
+/**
+ * GET /
+ * show available endpoints
+ */
+router.get('/', function (req, res) {
+  res.json(callbackFactory.documentationCallback({ subroutes: ['login', 'logout', 'verify'] }))
+})
+
+/**
+ * GET verify
+ * returns documentation on how to use the verify route
+ */
+router.get('/verify', function (req, res) {
+  res.json(callbackFactory.documentationCallback({ usage: {
+    userID: '[userID]',
+    token: '[token]'
+  }}))
+})
+
+/**
+ * POST verify
+ * verifies a userID-token-combination
+ *
+ * Header: database
+ * Body: userID, token
+ */
+router.post('/verify', function (req, res) {
+
+  auth.verify(req.body.userID, req.body.token, req.get('Database'), function (result) {
+    res.json(result)
+  })
+})
+
+/**
+ * GET /login
+ * returns documentation on how to use the login route
+ */
+router.get('/login', function (req, res) {
+  res.json(callbackFactory.documentationCallback({ usage: {
+    mail: '[mail]',
+    password: '[password]'
+  }}))
+})
+
+/**
+ * POST /login
+ * creates a new 30-day token for the user
+ *
+ * Header: database
+ * Body: mail, password
+ */
+router.post('/login', function (req, res) {
+
+  auth.login(req.body.mail, req.body.password, req.get('Database'), function (result) {
+    res.json(result)
+  })
+})
+
+/**
+ * GET /logout
+ * returns documentation on how to use the logout route
+ */
+router.get('/logout', function (req, res) {
+  res.json(callbackFactory.documentationCallback({ usage: {
+    userID: '[userID]',
+    token: '[token]'
+  }}))
+})
+
+/**
+ * POST /logout
+ * invalidates a token
+ *
+ * Header: database
+ * Body: userID, token
+ */
+router.post('/logout', function (req, res) {
+  auth.logout(req.body.userID, req.body.token, req.get('Database'), function (result) {
+    res.json(result)
+  })
+})
