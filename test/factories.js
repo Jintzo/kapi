bf4f4605d74f9f71fd136cf3aba323360b0742d6
Jintzo <me@jintzo.me>
2017-03-19 14:43:44 +0100

    added single datapoint callback tests

--- test/factories.js
+++ test/factories.js
@@ -14,7 +14,7 @@ describe('Callback Factory', function () {
     it('should return default error if both errorText and responseType are undefined', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.errorCallback(null, null)
+      const returnObject = callbackFactory.error(null, null)
 
       // basic checks
       should.exist(returnObject)
@@ -41,7 +41,7 @@ describe('Callback Factory', function () {
     it('should return default error if both errorText and responseType are empty', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.errorCallback('', '')
+      const returnObject = callbackFactory.error('', '')
 
       // basic checks
       should.exist(returnObject)
@@ -68,7 +68,7 @@ describe('Callback Factory', function () {
     it('should return adapted default error if only errorText is set', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.errorCallback('errorText', '')
+      const returnObject = callbackFactory.error('errorText', '')
 
       // basic checks
       should.exist(returnObject)
@@ -95,7 +95,7 @@ describe('Callback Factory', function () {
     it('should return adapted default error if only responseType is set', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.errorCallback('', 'response-test')
+      const returnObject = callbackFactory.error('', 'response-test')
 
       // basic checks
       should.exist(returnObject)
@@ -122,7 +122,7 @@ describe('Callback Factory', function () {
     it('should return custom error if all is correctly set', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.errorCallback('errorText', 'response-test')
+      const returnObject = callbackFactory.error('errorText', 'response-test')
 
       // basic checks
       should.exist(returnObject)
@@ -152,7 +152,7 @@ describe('Callback Factory', function () {
     it('should return empty documentation if no documentationObject is defined', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.documentationCallback(null)
+      const returnObject = callbackFactory.documentation(null)
 
       // basic checks
       should.exist(returnObject)
@@ -171,7 +171,7 @@ describe('Callback Factory', function () {
     it('should return empty documentation if documentation is not an object', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.documentationCallback('test')
+      const returnObject = callbackFactory.documentation('test')
 
       // basic checks
       should.exist(returnObject)
@@ -190,7 +190,7 @@ describe('Callback Factory', function () {
     it('should return adapted documentation if documentationObject is correctly defined', function (done) {
 
       // get return object
-      const returnObject = callbackFactory.documentationCallback({foo: 'bar'})
+      const returnObject = callbackFactory.documentation({foo: 'bar'})
 
       // basic checks
       should.exist(returnObject)
@@ -208,6 +208,135 @@ describe('Callback Factory', function () {
       returnObject.data.attributes.should.have.property('foo')
       returnObject.data.attributes.foo.should.be.a('string')
       returnObject.data.attributes.foo.should.equal('bar')
+      done()
+    })
+  })
+
+  describe('#single(dataObject, responseType)', function () {
+
+    it('should return null attributes if no attributes are specified', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single(null, null)
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // attribute checks
+      returnObject.data.should.have.property('attributes')
+      should.equal(returnObject.data.attributes, null)
+
+      done()
+    })
+
+    it('should return null attributes if specified attributes is not an object', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single('abc', null)
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // attribute checks
+      returnObject.data.should.have.property('attributes')
+      should.equal(returnObject.data.attributes, null)
+
+      done()
+    })
+
+    it('should return correct attributes if valid attributes are specified', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single({ foo: 'bar' }, null)
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // attribute checks
+      returnObject.data.should.have.property('attributes')
+      returnObject.data.attributes.should.be.a('object')
+      returnObject.data.attributes.should.have.property('foo')
+      returnObject.data.attributes.foo.should.be.a('string')
+      returnObject.data.attributes.foo.should.equal('bar')
+
+      done()
+    })
+
+    it('should return general response if no responseType is specified', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single(null, null)
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // type checks
+      returnObject.data.should.have.property('type')
+      returnObject.data.type.should.be.a('string')
+      returnObject.data.type.should.equal('response-general')
+
+      done()
+    })
+
+    it('should return general response if specified responseType is not a string', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single(null, { foo: 'bar' })
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // type checks
+      returnObject.data.should.have.property('type')
+      returnObject.data.type.should.be.a('string')
+      returnObject.data.type.should.equal('response-general')
+
+      done()
+    })
+
+    it('should return correct response type if valid responseType is specified', function (done) {
+
+      // get return object
+      const returnObject = callbackFactory.single(null, 'response-test')
+
+      // basic checks
+      should.exist(returnObject)
+      returnObject.should.be.a('object')
+
+      // structure checks
+      returnObject.should.have.property('data')
+      returnObject.data.should.be.a('object')
+
+      // type checks
+      returnObject.data.should.have.property('type')
+      returnObject.data.type.should.be.a('string')
+      returnObject.data.type.should.equal('response-test')
+
       done()
     })
   })
@@ -270,21 +399,29 @@ describe('Error Factory', function () {
     it('should return false if non-object is provided', function (done) {
       const result = errorFactory.containsError('abc')
       result.should.equal(false)
+
+      done()
     })
 
     it('should return false if random object is provided', function (done) {
       const result = errorFactory.containsError({ data: { usage: { test: '[test]' } } })
       result.should.equal(false)
+
+      done()
     })
 
     it('should return false if none-error is provided', function (done) {
       const result = errorFactory.containsError({ errors: [{ title: 'response-validate', detail: 'none' }] })
       result.should.equal(false)
+
+      done()
     })
 
     it('should return true if valid error object (!= none) is provided', function (done) {
       const result = errorFactory.containsError({ errors: [{ title: 'response-validate', detail: 'test' }] })
       result.should.equal(true)
+
+      done()
     })
   })
 })
