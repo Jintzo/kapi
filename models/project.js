2756da4c1655c89231b265ac151dad6579833f33
Jintzo <me@jintzo.me>
2017-06-21 23:47:22 +0200

    fix data building logic

--- models/project.js
+++ models/project.js
@@ -101,6 +101,7 @@ module.exports = {
                     description: sampleDescription
                   })
                 }
+                oldID = rows[i].projectID
               }
 
               // add tangling project
